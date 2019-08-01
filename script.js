54ff260294d79d1fbeaf41c82d2c9c98a3efd2eb
karen queiroz <kqueiroz812@gmail.com>
2019-08-01 15:28:26 -0300

    criar area de login

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-let email = document.querySelector ("email");
-let nome = document.querySelector ("nome");
-let confirmacao = document.querySelector ("confirmacao");
-let senha = document.querySelector ("senha");
-let botao = document.querySelector ("botao");
-let sexo = document.querySelector ("sexo");
+let email = document.querySelector ("#email");
+let nome = document.querySelector ("#nome");
+let confirmacao = document.querySelector ("#confirmacao");
+let senha = document.querySelector ("#senha");
+let botao = document.querySelector ("#botao");
+let sexo = document.querySelector ("#sexo");
+let div = document.querySelector ("div");
+// let texto = document.querySelector ("h2");
+// let naosou = document.querySelector ("a");
+
 
 
 function login(){
-    if(email === confirmacao){
-
+    if(email.value !== "" && email.value === confirmacao.value){
+        
+        localStorage.setItem("sexo", sexo.value)
+        localStorage.setItem("email", email.value)
+        localStorage.setItem("confirmacao", confirmacao.value)
+        localStorage.setItem("senha", senha.value)
+        localStorage.setItem("nome", nome.value)
+        // texto.inner.HTML = `Seja bem vindo ${nome}!`
+        // naosou.inner.HTML = `Não sou ${localStorage.nome}`
     }
-    localStorage.setItem("sexo", sexo.value)
-    localStorage.setItem("email", email.value)
-    localStorage.setItem("confirmacao", confirmacao.value)
-    localStorage.setItem("senha", senha.value)
-    localStorage.setItem("nome", nome.value)
+    
+    else{
+        ("Senha ou email invalido")
+    }
+
 }
+
 
 botao.onclick = login;
